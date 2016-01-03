4a14ce5e141114361db2becade0839f5cda91f67
Kyle Kelley <rgbkrk@gmail.com>
2016-01-02 21:58:14 -0600

    Test images too.

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -1,47 +1,40 @@
 import React from 'react';
 
-const defaultMimeTypes = ['image/png', 'image/jpeg', 'image/gif'];
-
 class ImageDisplay extends React.Component {
   static displayName = 'ImageDisplay'
 
   static propTypes = {
     data: React.PropTypes.string,
-    mimetype: React.PropTypes.oneOf(defaultMimeTypes).isRequired,
   }
 
   render() {
-    const mimetype = this.props.mimetype;
     const data = this.props.data;
     return (
-      <img src={'data:' + mimetype + ';base64,' + data} />
+      <img src={'data:' + this.mimetype + ';base64,' + data} />
     );
   }
 }
 
 export class PNGDisplay extends ImageDisplay {
   static displayName = 'PNGDisplay'
-  getDefaultProps() {
-    return {
-      mimetype: 'image/png',
-    };
+  constructor(props) {
+    super(props)
+    this.mimetype = 'image/png';
   }
 }
 
 export class JPEGDisplay extends ImageDisplay {
   static displayName = 'JPEGDisplay'
-  getDefaultProps() {
-    return {
-      mimetype: 'image/jpeg',
-    };
+  constructor(props) {
+    super(props)
+    this.mimetype = 'image/jpeg';
   }
 }
 
 export class GIFDisplay extends ImageDisplay {
   static displayName = 'GIFDisplay'
-  getDefaultProps() {
-    return {
-      mimetype: 'image/gif',
-    };
+  constructor(props) {
+    super(props)
+    this.mimetype = 'image/gif';
   }
 }
