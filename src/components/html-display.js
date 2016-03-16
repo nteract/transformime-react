ed135767ae1bd176ff3fe705cd1125f98e2fc6d8
Kyle Kelley <rgbkrk@gmail.com>
2016-03-16 07:21:26 -0500

    Make HTMLDisplay a pure function

--- src/components/html-display.js
+++ src/components/html-display.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-export default class HTMLDisplay extends React.Component {
-  static propTypes = {
-    data: React.PropTypes.string,
-  }
-
-  render() {
-    return (
-      <div
-        dangerouslySetInnerHTML={{ // eslint-disable-line
-          __html: this.props.data,
-        }}
-      />
-    );
-  }
+export default function HTMLDisplay(props) {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ // eslint-disable-line
+        __html: props.data,
+      }}
+    />
+  );
 }
+
+HTMLDisplay.propTypes = {
+  data: React.PropTypes.string.isRequired,
+};
