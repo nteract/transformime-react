148ce4e37d9d5dcafc41cd4c4403b39fcfb54881
Kyle Kelley <rgbkrk@gmail.com>
2016-03-16 07:22:38 -0500

    Make TextDisplay a pure function

--- src/components/text-display.js
+++ src/components/text-display.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export default class TextDisplay extends React.Component {
-  static propTypes = {
-    data: React.PropTypes.string,
-  }
-
-  render() {
-    return (
-      <pre>{this.props.data}</pre>
-    );
-  }
+export default function TextDisplay(props) {
+  return (
+    <pre>{props.data}</pre>
+  );
 }
+
+TextDisplay.propTypes = {
+  data: React.PropTypes.string,
+};
