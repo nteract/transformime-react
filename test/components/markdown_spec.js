25702360f676487043c2348838fb6d1099049346
Kyle Kelley <rgbkrk@gmail.com>
2016-03-16 09:48:07 -0500

    Address Markdown rendering.

--- test/components/markdown_spec.js
+++ test/components/markdown_spec.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import { expect } from 'chai';
 
-import {
-  renderIntoDocument,
-} from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 
 import MarkdownDisplay from '../../src/components/markdown-display';
 
 describe('MarkdownDisplay', () => {
   it('renders some markdown', () => {
-    const component = renderIntoDocument(
+    const component = shallow(
       <MarkdownDisplay data={'# Heading\nWoo.'} />
     );
 
-    const el = ReactDOM.findDOMNode(component);
-    expect(el).to.not.be.null;
-    expect(el.firstChild.localName).to.equal('h1');
-    expect(el.firstChild.textContent).to.equal('Heading');
-    expect(el.lastChild.localName).to.equal('p');
-    expect(el.lastChild.textContent).to.equal('Woo.');
+    expect(component.node.props.source).to.equal('# Heading\nWoo.');
   });
 });
