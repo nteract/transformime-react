554a9f820161450bbd4f361dfe655e9cbc702b11
Kyle Kelley <rgbkrk@gmail.com>
2016-03-16 10:48:03 -0500

    Test that katex was used underneath.
    No idea how to do this one well.

--- test/components/latex_spec.js
+++ test/components/latex_spec.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { expect } from 'chai';
 
-import {
-  renderIntoDocument,
-} from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 
 import LaTeXDisplay from '../../src/components/latex-display';
 
 describe('LaTeXDisplay', () => {
   it('processes basic LaTeX', () => {
-    const component = renderIntoDocument(
+    const component = shallow(
       <LaTeXDisplay data={'x^2 + y = 3'} />
     );
 
-    const el = ReactDOM.findDOMNode(component);
-    expect(el).to.not.be.null;
-    expect(el.innerHTML).to.include('math');
-    expect(el.innerHTML).to.include('x^2 + y = 3');
-    // Ok, it would have a bunch more, but I'm not testing KaTeX itself
+    const rendered = component.render();
+    expect(rendered[0].children[0].children[0].attribs.class).to.equal('katex');
   });
 });
